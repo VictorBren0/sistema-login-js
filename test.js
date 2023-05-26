2773dec703a75854784446391e450b429fddeb9c
VictorBren0 <victor.bsr2000@gmail.com>
2023-05-26 15:43:51 -0300

    fix: removed accent from test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 QUnit.test("Teste adicionarUsuario - Usuário já cadastrado", function(assert) {
-  var usuarios = [{ Nome: "João", Senha: "12345678" }];
+  var usuarios = [{ Nome: "Joao", Senha: "12345678" }];
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  $("#txtNome").val("João");
+  $("#txtNome").val("Joao");
   $("#txtSenha").val("12345678");
   var result = adicionarUsuario();
   assert.strictEqual(result, false, "Retorna falso quando o usuário já está cadastrado");
@@ -21,7 +21,7 @@ QUnit.test("Teste adicionarUsuario - Nome com menos de 3 caracteres", function(a
 QUnit.test("Teste adicionarUsuario - Senha com menos de 8 caracteres", function(assert) {
   var usuarios = [];
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  $("#txtNome").val("João");
+  $("#txtNome").val("Joao");
   $("#txtSenha").val("1234567");
   var result = adicionarUsuario();
   assert.strictEqual(result, false, "Retorna falso quando a senha tem menos de 8 caracteres");
@@ -31,25 +31,25 @@ QUnit.test("Teste adicionarUsuario - Senha com menos de 8 caracteres", function(
 QUnit.test("Teste adicionarUsuario - Usuário registrado com sucesso", function(assert) {
   var usuarios = [];
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  $("#txtNome").val("João");
+  $("#txtNome").val("Joao");
   $("#txtSenha").val("12345678");
   var result = adicionarUsuario();
   assert.strictEqual(result, true, "Retorna verdadeiro quando o usuário é registrado com sucesso");
   assert.strictEqual($("#mensagem").text(), "Usuário registrado com sucesso.", "Exibe a mensagem correta");
-  assert.strictEqual(localStorage.getItem("usuarios"), JSON.stringify([{ Nome: "João", Senha: "12345678" }]), "Usuário é adicionado ao localStorage");
+  assert.strictEqual(localStorage.getItem("usuarios"), JSON.stringify([{ Nome: "Joao", Senha: "12345678" }]), "Usuário é adicionado ao localStorage");
 });
 
 QUnit.test("Teste verificarLogin - Login bem-sucedido", function(assert) {
-  var usuarios = [{ Nome: "João", Senha: "12345678" }];
+  var usuarios = [{ Nome: "Joao", Senha: "12345678" }];
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  $("#txtLoginNome").val("João");
+  $("#txtLoginNome").val("Joao");
   $("#txtLoginSenha").val("12345678");
   verificarLogin();
   assert.strictEqual($("#mensagem").text(), "Login bem-sucedido.", "Exibe a mensagem correta");
 });
 
 QUnit.test("Teste verificarLogin - Nome de usuário ou senha inválidos", function(assert) {
-  var usuarios = [{ Nome: "João", Senha: "12345678" }];
+  var usuarios = [{ Nome: "Joao", Senha: "12345678" }];
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
   $("#txtLoginNome").val("Maria");
   $("#txtLoginSenha").val("12345678");
